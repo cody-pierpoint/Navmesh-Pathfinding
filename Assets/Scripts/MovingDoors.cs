05f952e158320008896b2044b1356b572b370ca5
cody-pierpoint <60990371+cody-pierpoint@users.noreply.github.com>
2021-05-19 12:45:33 +1000

    Wall moving complete, main menu and paused menu completed

--- Assets/Scripts/MovingDoors.cs
+++ Assets/Scripts/MovingDoors.cs
@@ -6,32 +6,51 @@ using UnityEngine.AI;
 
 public class MovingDoors : MonoBehaviour
 {
-    [SerializeField] GameObject[] movingDoors;
+    [SerializeField] GameObject movingDoors;
     [SerializeField] float moveDistance = 4f;
     private bool moveBack = false;
-    private Transform startingPoint;
-    private Transform endingPos;
+    [SerializeField]private Transform startingPoint;
+    [SerializeField]private Transform endingPos;
     private int distance;
+    [SerializeField] private float speed;
+    private float velocity;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        startingPoint.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //startingPoint.transform.position =  new Vector3(transform.position.x, transform.position.y, transform.position.z);
         gameObject.GetComponent<NavMeshObstacle>();
+        //  endingPos.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 4f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (GameObject doors in movingDoors)
-        {
-            if(!moveBack)
-            {
-                //endingPos.transform.position = transform.position;
-
-                transform.Translate(transform.right * (Mathf.PingPong(movingDoors[movingDoors.Length].transform.position, moveDistance)));
-            }
-            
-        }
+        //transform.position = Vector3.SmoothDamp(transform.position, endingPos.transform.position, ref , moveDistance, speed); ;
+        //if(Mathf.Abs(transform.position.z - endingPos.transform.position.z) < 0.25)
+        //{
+        //    endingPos = endingPos == startingPoint ? endingPos : startingPoint;
+        //}
+        //transform.Translate(-transform.right * speed * Time.deltaTime);
+        //if ()
+        //{
+        //    transform.Translate(transform.right * speed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //   transform.Translate(-transform.right * speed * Time.deltaTime);
+        //}
+        //if (!moveBack && transform.position == endingPos.transform.position)
+        //{
+           
+        //    moveBack = false;
+        //}
+        //else
+        //{
+        //    transform.Translate(Vector3.MoveTowards(transform.position, endingPos.transform.position, 0f) * speed * Time.deltaTime);
+        //    moveBack = true;
+        //}
     }
 }
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public GameObject pausePanel;
+    private bool isPaused = false;
+    private bool IsPaused
+    {
+        get
+        {
+            return isPaused;
+        }
+        set
+        {
+            isPaused = value;
+            pausePanel.SetActive(value);
+            Time.timeScale = value ? 0 : 1;
+        }
+    }
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IsPaused = !IsPaused;    
+        }
+    }
+    
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+
+    }
+
+
+    public void togglepause()
+    {
+        IsPaused = !IsPaused;
+    }
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
+        Application.Quit();
+    }
+
+}
